776f5c312b87c6c450b7bedb60929e7b1b0ccd5b
MaxPathak <67355216+TragicNet@users.noreply.github.com>
2021-08-26 00:21:14 +0530

    auto hide navigation buttons

--- app/src/main/java/com/example/reon/activities/BaseActivity.java
+++ app/src/main/java/com/example/reon/activities/BaseActivity.java
@@ -28,9 +28,9 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         this.app = ((Reon) this.getApplication());
-        // Auto Hide navigation buttons
+        // Auto Hide Navigation buttons
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
